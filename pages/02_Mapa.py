50365a2960f5f3c0e0c7944c5bc5a52f0d632584
Lautaro <lautaro.atler99@gmail.com>
2026-06-06 14:01:18 -0300

    agregue parametros

--- pages/02_Mapa.py
+++ pages/02_Mapa.py
@@ -1,1 +1,14 @@
 import streamlit as st
+
+#los niveles se representan por piso, cada piso tiene 
+#atributos (salas, baños, etc). cada atributo tiene 4 coordenadas
+#se accede por ej: niveles[planta_baja][{atributo}][{area}]
+niveles={
+    planta_baja={
+
+    },
+    nivel_1={},
+    nivel_2={},
+    techo={}
+}
+
