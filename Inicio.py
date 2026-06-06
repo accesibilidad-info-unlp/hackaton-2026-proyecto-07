f45341a47d066f4bb9abd0f4b796419a4c8e2ff6
Ana Freitas <ana.freitaas16@gmail.com>
2026-06-06 13:02:11 -0300

    agregue paginas

--- Inicio.py
+++ Inicio.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.write("hola")
+
+st.set_page_config(page_title="Inicio", layout="wide")
+
+pag_inicio = st.Page("pages/00_Home.py", title="Inicio")
+pag_Images = st.Page("pages/01_Images.py", title="Imagens")
+pag_Mapa = st.Page("pages/02_Mapa.py", title="Mapa")
+
+pg = st.navigation({
+    "Principal": [pag_inicio,],
+    "Herramientas": [pag_Images, pag_Mapa],
+})
+
+pg.run()
